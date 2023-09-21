1f05d95dff4180bd413eb364587162a09b269b98
Felipe Schein <scheinfelipe@gmail.com>
2023-09-21 14:15:59 -0300

    Update

--- views.py
+++ views.py
@@ -4,3 +4,7 @@ from flask import render_template
 @application.route('/')
 def home():
     return render_template('home.html')
+
+@application.route('/dash')
+def dash():
+    return render_template('dashboard.html')
